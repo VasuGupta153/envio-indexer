bc66894520194798ec5262bc7a233dd07db84743
vasu gupta <vasugupta153@gmail.com>
2024-09-07 16:49:26 +0530

    Subgraph modified

--- hyperharvest/src/EventHandlers.ts
+++ hyperharvest/src/EventHandlers.ts
@@ -4,18 +4,11 @@ import {
   HyperHarvest_UserWithdrawn,
   HyperHarvest_WithdrawBridgeAndSupplied,
 } from "generated";
-import { queueService } from './queueService';
+
 
 // Ensure the queue service is connected before processing events
 let queueConnected = false;
 
-async function ensureQueueConnection() {
-  // console.log('hi')
-  if (!queueConnected) {
-    await queueService.connect();
-    queueConnected = true;
-  }
-}
 
 HyperHarvest.UserDeposited.handler(async ({ event, context }) => {
   const entity: HyperHarvest_UserDeposited = {
@@ -26,15 +19,6 @@ HyperHarvest.UserDeposited.handler(async ({ event, context }) => {
   };
 
   context.HyperHarvest_UserDeposited.set(entity);
-
-  // Publish event to CloudAMQP
-  await ensureQueueConnection();
-  await queueService.publishMessage('blockchain-events', {
-    type: 'UserDeposited',
-    user: event.params.user,
-    assets: event.params.assets.toString(),
-    shares: event.params.shares.toString(),
-  });
 });
 
 HyperHarvest.UserWithdrawn.handler(async ({ event, context }) => {
@@ -47,14 +31,6 @@ HyperHarvest.UserWithdrawn.handler(async ({ event, context }) => {
 
   context.HyperHarvest_UserWithdrawn.set(entity);
 
-  // Publish event to CloudAMQP
-  await ensureQueueConnection();
-  await queueService.publishMessage('blockchain-events', {
-    type: 'UserWithdrawn',
-    user: event.params.user,
-    assets: event.params.assets.toString(),
-    shares: event.params.shares.toString(),
-  });
 });
 
 HyperHarvest.WithdrawBridgeAndSupplied.handler(async ({ event, context }) => {
@@ -67,12 +43,4 @@ HyperHarvest.WithdrawBridgeAndSupplied.handler(async ({ event, context }) => {
 
   context.HyperHarvest_WithdrawBridgeAndSupplied.set(entity);
 
-  // Publish event to CloudAMQP
-  await ensureQueueConnection();
-  await queueService.publishMessage('blockchain-events', {
-    type: 'WithdrawBridgeAndSupplied',
-    receiver: event.params.Receiver,
-    gasFeesAmount: event.params.GasFeesAmount.toString(),
-    destinationChainSelector: event.params.DestinationChainSelector.toString(),
-  });
 });
